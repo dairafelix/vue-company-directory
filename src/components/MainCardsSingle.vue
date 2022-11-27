613318254cbb92836848ec070ae433b978f25245
Daira Felix <dairafelix@icloud.com>
2022-11-26 20:15:59 -0600

    Added Employee Data and Department

--- src/components/MainCardsSingle.vue
+++ src/components/MainCardsSingle.vue
@@ -1,12 +1,31 @@
 <script setup>
+  import { ref } from 'vue'
   import { faker } from '@faker-js/faker'
-  const firstName = faker.name.firstName()
-  const lastName = faker.name.lastName()
-  const fullName = `${firstName} ${lastName}`
-
+  import useAPI from '@/composables/useAPI'
+  const { getDepartment } = useAPI()
   const selectCard = () => {
-    console.log(`${fullName} selected`)
+    console.log(`${props.employee.name} selected`)
   }
+  const props = defineProps({
+    employee: {
+      type: Object,
+      required: true,
+      default: () => {
+        return {
+          createdAt: '2022-01-01',
+          departmentId: '123',
+          email: 'john.doe@example.com',
+          employeeId: '123',
+          name: 'John Doe',
+          quote: 'Really Cool quote',
+          title: 'Position',
+          updatedAt: '2022-01-01',
+        }
+      },
+    },
+  })
+  const departmentResponse = await getDepartment(props.employee.departmentId)
+  const department = ref(departmentResponse)
 </script>
 
 <template>
@@ -15,35 +34,27 @@
       <img :src="faker.internet.avatar()" alt="" srcset="" />
     </div>
     <div class="card-details">
-      <p class="card-details-name">{{ fullName }}</p>
-      <p class="card-details-job">{{ faker.name.jobTitle() }}</p>
-      <!-- Clicking on the email tab will open a new email window 😄 -->
-      <a class="card-details-email" :href="'mailto:' + faker.internet.exampleEmail(fullName)">{{
-        faker.internet.exampleEmail(fullName)
-      }}</a>
-      <p class="card-details-quote">"{{ faker.lorem.paragraph() }}"</p>
+      <p class="card-details-name">{{ props.employee.name }}</p>
+      <p class="card-details-job">{{ props.employee.title }}, {{ department.name }}</p>
+      <p class="card-details-quote">"{{ props.employee.quote }}"</p>
     </div>
   </div>
 </template>
-
 <style scoped lang="postcss">
   .card {
-    @apply cursor-pointer overflow-hidden rounded-md bg-slate-100 p-8 shadow-md transition duration-300 hover:scale-110 hover:shadow-slate-900;
+    @apply cursor-pointer overflow-hidden rounded-md bg-slate-100 p-8 shadow-md transition-transform duration-300 hover:scale-110 hover:shadow-2xl hover:shadow-slate-900;
     &-image {
       img {
         @apply mx-auto rounded-full object-contain;
       }
     }
     &-details {
-      @apply flex flex-col gap-2 pt-6 text-center;
+      @apply flex flex-col gap-2  pt-6 text-center;
       &-name {
-        @apply text-3xl font-thin tracking-wider text-slate-800;
+        @apply text-3xl font-thin  tracking-wider text-slate-800;
       }
       &-job {
         @apply -mt-2 text-xs font-bold text-yellow-700;
-      }
-      &-email {
-        @apply text-sm text-blue-400;
       }
       &-quote {
         @apply pt-4 text-lg italic text-slate-800;
